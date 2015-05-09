bbe6db11dbbf4e487762c608125529d28527d052
evanhackett <evanwhackett@gmail.com>
2015-05-09 14:12:24 -0700

    added HR banner

---
+++ javascripts/HRbanner.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('body').append(
+      '<a href="http://hackreactor.com"> \
+      <img style="position: fixed; top: 0; right: 0; border: 0;" \
+      src="http://i.imgur.com/x86kKmF.png" \
+      alt="Built at Hack Reactor"> \
+      </a>'
+    );
+});
