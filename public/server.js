1b9079caa1f4afec83682ed1d9d3c29190e68aad
Phoebe Li <phoebeli@Phoebes-MacBook-Pro.local>
2015-05-11 17:40:13 -0400

    implemented framework for continuous deployment to azure

--- public/server.js
+++ public/server.js
@@ -1,20 +1,20 @@
-// silly chrome wants SSL to do screensharing
+
 var fs = require('fs'),
     express = require('express'),
     https = require('https'),
     http = require('http'),
     path = require('path');
 
-// var privateKey = fs.readFileSync('fakekeys/privatekey.pem').toString(),
-//     certificate = fs.readFileSync('fakekeys/certificate.pem').toString();
 
 
 var app = express();
 
 app.use(express.static(__dirname));
 
-// https.createServer({key: privateKey, cert: certificate}, app).listen(8000);
-http.createServer(app).listen(8000);
+//setup environment port variable
+//default to port 4000
+var port = process.env.PORT || 4000;
+http.createServer(app).listen(port);
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/index.html'));
